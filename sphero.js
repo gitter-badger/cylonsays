d04a8c940ee872dd66623c01fad3f0b2f1aa4203
Dave Welch <david@davidwelch.co>
2014-01-15 21:24:09 -0700

    Updates...

--- sphero.js
+++ sphero.js
@@ -1,47 +1,68 @@
 var Cylon = require('cylon');
 
 Cylon.robot({
-  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-ROR-AMP-SPP' },
+  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-RWY-AMP-SPP' },
   device: {name: 'sphero', driver: 'sphero'},
 
   work: function(my) {
   	var s = my.sphero;
+  	s.setBackLED(100);
 
 
 	console.log("Connected...");
-
-	var col = 0xFF0000;
-
-    var stop = false;
-	s.setRGB([col]);
-
-
-
-	// s.detectCollisions();
-
-	// every((1).second(), function() { 
- //    	// s.roll(60, Math.floor(Math.random() * 360));
- //    	col = (col & 0xfefefe) << 1;
- //    	s.setRGB([col]);
- //    });
+	s.setColor('green');
 
 	process.stdin.resume();
 	process.stdin.setEncoding('utf8');
 	var util = require('util');
 
+	var redCount = 0, blueCount = 0;
+
 	var running = false;
-	process.stdin.on('data', function (text) {
-		text = util.inspect(text);
-		console.log('received data:', text);
+	process.stdin.on('data', function (chunk) {
+		text = chunk.toString(); // util.inspect(chunk);
+		console.log(chunk, ' - received data:', text);
 
-		var direction = text == "r\n" ? 120 : 240;
+		console.log("Text: ", text);
+		var dir;
 
-		if( running ){
-			s.stop();
-		}else{
-			s.roll(60, 120);
+		switch(text[0]){
+			case '\n':
+				s.stop();
+				return;
+
+			case 'r':
+				redCount++;
+				dir = 0;
+				break;
+			case 'b':
+				blueCount++;
+				dir = 180;
+				break;
+
+			case 'z':
+				throw "Blah!";
+
+			default: return console.log("Failboat:", text);
 		}
-		running = !running;
+
+		console.log("Red:", redCount, "Blue:", blueCount);
+
+
+		if( redCount == blueCount ){
+			s.stop();
+			s.setColor('green');
+			return;
+		}else if(redCount > blueCount){
+			dir = 1;
+			s.setColor('red');
+		}else{
+			dir = 181;
+			s.setColor('blue');
+		}
+
+		console.log("Moving: ", dir);
+		s.roll(60, dir);
 	});
   }
 }).start();
