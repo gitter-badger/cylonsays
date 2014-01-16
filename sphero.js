37df8dd95fc66e1f4ad40353c82898001c362217
Dave Welch <david@davidwelch.co>
2014-01-15 20:33:08 -0700

    Updates...

--- sphero.js
+++ sphero.js
@@ -1,12 +1,44 @@
 var Cylon = require('cylon');
 
 Cylon.robot({
-  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/tty.Sphero-RGR-AMP-SPP' },
+  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-ROR-AMP-SPP' },
   device: {name: 'sphero', driver: 'sphero'},
 
   work: function(my) {
-    every((1).second(), function() { 
-      my.sphero.roll(60, Math.floor(Math.random() * 360));
-    });
+  	var s = my.sphero;
+
+
+	console.log("Connected...");
+
+	var col = 0xFF0000;
+
+    var stop = false;
+	s.setRGB([col]);
+
+
+
+	// s.detectCollisions();
+
+	// every((1).second(), function() { 
+ //    	// s.roll(60, Math.floor(Math.random() * 360));
+ //    	col = (col & 0xfefefe) << 1;
+ //    	s.setRGB([col]);
+ //    });
+
+	process.stdin.resume();
+	process.stdin.setEncoding('utf8');
+	var util = require('util');
+
+	var running = false;
+	process.stdin.on('data', function (text) {
+	console.log('received data:', util.inspect(text));
+
+	if( running ){
+		s.stop();
+	}else{
+		s.roll(60, 100);
+	}
+		running = !running;
+	});
   }
 }).start();
