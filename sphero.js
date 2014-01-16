835e55f34c6c88b7f4289601294e32031130c38a
Dave Welch <david@davidwelch.co>
2014-01-15 21:31:30 -0700

    Updates...

--- sphero.js
+++ sphero.js
@@ -1,54 +1,19 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var util = require('util');
 var Cylon = require('cylon');
 
-Cylon.robot({
-  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-RWY-AMP-SPP' },
-  device: {name: 'sphero', driver: 'sphero'},
-
-  work: function(my) {
-  	var s = my.sphero;
-  	s.setBackLED(100);
-
+function workFn(my){
+  	var s = my.sphero,
+  		redCount = 0, 
+  		blueCount = 0;
+  	
 
 	console.log("Connected...");
 	s.setColor('green');
+	s.setBackLED(100);
 
-	process.stdin.resume();
-	process.stdin.setEncoding('utf8');
-	var util = require('util');
-
-	var redCount = 0, blueCount = 0;
-
-	var running = false;
-	process.stdin.on('data', function (chunk) {
-		text = chunk.toString(); // util.inspect(chunk);
-		console.log(chunk, ' - received data:', text);
-
-		console.log("Text: ", text);
-		var dir;
-
-		switch(text[0]){
-			case '\n':
-				s.stop();
-				return;
-
-			case 'r':
-				redCount++;
-				dir = 0;
-				break;
-			case 'b':
-				blueCount++;
-				dir = 180;
-				break;
-
-			case 'z':
-				throw "Blah!";
-
-			default: return console.log("Failboat:", text);
-		}
-
-		console.log("Red:", redCount, "Blue:", blueCount);
-
-
+	function roll(){
 		if( redCount == blueCount ){
 			s.stop();
 			s.setColor('green');
@@ -61,8 +26,44 @@ Cylon.robot({
 			s.setColor('blue');
 		}
 
-		console.log("Moving: ", dir);
 		s.roll(60, dir);
+		console.log("Moving: ", dir);
+	}
+
+
+	// replace this with the twitters...
+	process.stdin.on('data', function (chunk) {
+		text = chunk.toString();
+		// console.log(chunk, ' - received data:', text);
+		// console.log("Text: ", text);
+		var dir;
+
+		switch(text[0]){
+			case '\n':
+				s.stop();
+				return;
+
+			case 'r':
+				redCount++;
+				break;
+			case 'b':
+				blueCount++;
+				break;
+
+			case 'z':
+				throw "Blah!";
+
+			default: return console.log("Failboat:", text);
+		}
+
+		console.log("Red:", redCount, "Blue:", blueCount);
+		roll();
 	});
-  }
+};
+
+
+Cylon.robot({
+	connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/cu.Sphero-RWY-AMP-SPP' },
+	device: {name: 'sphero', driver: 'sphero'},
+	work: workFn
 }).start();
