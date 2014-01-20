03f21b6733dd1368f51f61c50369b631baa976c2
Dan Langford <danlangford@gmail.com>
2014-01-20 11:33:05 -0700

    added some find replace as well as logging to std err

--- cylonsays.js
+++ cylonsays.js
@@ -19,10 +19,23 @@ var twitter = new Twit({
 
 var sayWat = function(item){
     if (!item.text) return;
-    console.log(item.text);
+    var msg = (item.text+"\n")
+        .replace("#cylonsays"," hashtag sighlon says ")
+        .replace("cylon","signlon");
+    
+    console.log(msg);
+    util.error(msg);
 };
 
+var logStdErr = function(x){
+    util.error(util.inspect(x));    
+}
 
 var stream = twitter.stream('statuses/filter', {track:'#cylonsays'});
 stream.on('tweet', sayWat);
 
+stream.on('connect', logStdErr);
+stream.on('disconnect', logStdErr);
+stream.on('reconnect', logStdErr);
+stream.on('limit', logStdErr);
+stream.on('warning', logStdErr);
